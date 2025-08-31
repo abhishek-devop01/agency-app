bffa5fb90769b6a3fbc26bff2f3f2fc3a459b385
abhishek devop <standup787059@gmail.com>
2025-09-01 00:07:51 +0530

    completed TrustedBy component and created Services component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ theme, setTheme }) => {
      const [sidebarOpen, setsidebarOpen] = useState(false);
 
   return (
-    <div>
+    
       <div
         className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40
       py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70"
@@ -37,7 +37,7 @@ const Navbar = ({ theme, setTheme }) => {
           </a>
         </div>
       </div>
-    </div>
+    
   );
 };
 
---
+++ src/components/Services.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Services = () => {
+
+     const ServicesData = [
+          {
+               title: 'Advertising',
+               description: "we turn bold ideas to powerful digital solutions that connect, engage...",
+               icon:assets.ads_icon
+          },
+          {
+               title: 'Content marketing',
+               description: "we turn bold ideas to powerful digital solutions that connect, engage...",
+               icon:assets.ads_icon
+          },
+     ]
+  return (
+    <div></div>
+  )
+}
+
+export default Services
