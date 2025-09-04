1b141071c0322de517f8208a80dd99ddbab88cbc
abhishek devop <standup787059@gmail.com>
2025-09-04 23:52:48 +0530

    working on serviceCard

--- src/components/SeviceCard.jsx
+++ src/components/SeviceCard.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const SeviceCard = () => {
+const SeviceCard = ({service, index}) => {
   return (
-    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border
-    border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100'>
+    <div
+      className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border
+    border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100"
+    >
+      <div
+        className="pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blur-500 via-indigo-500
+     to-purple-500 w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten opacity-70"
+      >
+        <div className="flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5
+        transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative">
 
-     <div className='pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blur-500 via-indigo-500
-     to-purple-500 w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten opacity-70'>
+          <div className="bg-gray-100 dark:bg-gray-700 rounded-full">
+               <img src={service.icon} className="max-w-24 bg-white dark:bg-gray-900 rounded-full m-2" alt="" />
+          </div>
+          <div className="flex-1">
+               <h3>{service.title}</h3>
+          </div>
 
-     </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SeviceCard
+export default SeviceCard;
