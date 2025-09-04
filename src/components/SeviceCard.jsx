3663187a2b7b511f712c825b0451c4c4ff611f86
abhishek devop <standup787059@gmail.com>
2025-09-04 23:46:43 +0530

    serviceCard created

---
+++ src/components/SeviceCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SeviceCard = () => {
+  return (
+    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border
+    border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100'>
+
+     <div className='pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blur-500 via-indigo-500
+     to-purple-500 w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten opacity-70'>
+
+     </div>
+    </div>
+  )
+}
+
+export default SeviceCard
