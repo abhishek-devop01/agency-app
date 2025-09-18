b655efab887d86349e340ddef34758ece7f9c4b5
abhishek devop <standup787059@gmail.com>
2025-09-18 23:19:08 +0530

    completed UI

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -20,12 +20,44 @@ const ContactUs = () => {
           <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
             <img src={assets.person_icon} alt="" />
             <input
+              name="name"
               type="text"
               placeholder="Enter your name..."
               className="w-full p-3 text-sm outline-none"
+              required
             />
           </div>
         </div>
+        <div>
+          <p className="mb-2 text-sm font-medium">Email id</p>
+          <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+            <img src={assets.email_icon} alt="" />
+            <input
+              name="email"
+              type="text"
+              placeholder="example@gmail.com"
+              className="w-full p-3 text-sm outline-none"
+              required
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <p className="mb-2 text-sm font-medium">Message</p>
+          <textarea
+            name="message"
+            rows={8}
+            placeholder="Enter your message"
+            className="w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all"
+        >
+          Submit <img className="w-4" src={assets.arrow_icon} alt="" />
+        </button>
       </form>
     </div>
   );
