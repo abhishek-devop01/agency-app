d20c4597a1c1a54f438289eb90fd9d059f5e809c
abhishek devop <standup787059@gmail.com>
2025-09-18 22:59:15 +0530

    added title component

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import Title from "./Title";
 
 const ContactUs = () => {
   return (
-    <div id='contact-us' className=''>
-
+    <div
+      id="contact-us"
+      className="flex flex-col items-center gap-7 px-4 sm:px-12
+    lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
+    >
+      <Title
+        title="Reach out to us"
+        desc="From strategy to execution, we craft digital solution that move your business forward."
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
