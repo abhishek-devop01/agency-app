64de121c1dcd5b084886cab7206beeb645a7b499
abhishek devop <standup787059@gmail.com>
2025-09-18 23:06:12 +0530

    added form

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Title from "./Title";
+import assets from "../assets/assets";
 
 const ContactUs = () => {
   return (
@@ -12,6 +13,20 @@ const ContactUs = () => {
         title="Reach out to us"
         desc="From strategy to execution, we craft digital solution that move your business forward."
       />
+
+      <form className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
+        <div>
+          <p className="mb-2 text-sm font-medium">Your name</p>
+          <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+            <img src={assets.person_icon} alt="" />
+            <input
+              type="text"
+              placeholder="Enter your name..."
+              className="w-full p-3 text-sm outline-none"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
