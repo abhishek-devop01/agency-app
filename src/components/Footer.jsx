895661076678d2aa072c62356447976a9ba17f36
abhishek devop <standup787059@gmail.com>
2025-09-19 21:35:23 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import assets from "../assets/assets";
 
-const Footer = () => {
+const Footer = ({ theme }) => {
   return (
     <div className="bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40">
       {/* footer top */}
       <div className="flex justify-between lg:items-center max-lg:flex-col gap-10">
-        <div>
-          <img src={assets.logo} className="w-32 sm:w-44" alt="" />
+        <div className="space-y-5 text-sm text-gray-700 dark:text-gray-400">
+          <img
+            src={theme === "dark" ? assets.logo_dark : assets.logo}
+            className="w-32 sm:w-44"
+            alt=""
+          />
           <p className="max-w-md">
             From strategy to execution, we craft digital solutions that move
             your business forward.
           </p>
-          <ul>
+          <ul className="flex gap-8">
             <li>
               <a className="hover:text" href="#hero">
                 Home
@@ -35,7 +39,22 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div></div>
+        <div className="text-gray-600 dark:text-gray-400">
+          <h3 className="font-semibold">Subscribe to our newsletter</h3>
+          <p className="text-sm mt-2 mb-6">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+          <div className="flex gap-2 text-sm">
+            <input
+              placeholder="Enter your email"
+              type="text"
+              className="w-full p-3 text-sm outline-none rounded dark:text-gray-200 bg-transparent border border-gray-300 dark:border-gray-500"
+            />
+            <button className="bg-primary text-white rounded px-6">
+              Subscribe
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
