3c6c23080b0af591e71c4d42f5190e0c0ce77c01
abhishek devop <standup787059@gmail.com>
2025-09-20 19:13:47 +0530

    custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import TrustedBy from './components/TrustedBy'
-import Services from './components/Services'
-import OurWork from './components/OurWork'
-import Teams from './components/Teams'
-import ContactUs from './components/ContactUs'
-import { Toaster } from 'react-hot-toast'
-import Footer from './components/Footer'
+import React, { useRef, useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import TrustedBy from "./components/TrustedBy";
+import Services from "./components/Services";
+import OurWork from "./components/OurWork";
+import Teams from "./components/Teams";
+import ContactUs from "./components/ContactUs";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme'): 'light')
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+  
+  const dotRef = useRef(null);
+  const outlineRef = useRef(null);
+
+  const mouse = useRef({ x: 0, y: 0 });
+  const position = useRef({ x: 0, y: 0 });
+
   return (
-    <div className='dark:bg-black relative'>
+    <div className="dark:bg-black relative">
       <Toaster />
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
@@ -22,8 +31,21 @@ const App = () => {
       <Teams />
       <ContactUs />
       <Footer theme={theme} />
-    </div>
-  )
-}
 
-export default App
+      {/* custom cursor */}
+      <div
+        ref={outlineRef}
+        className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]"
+      ></div>
+
+      {/* custom cursor Dot */}
+
+      <div
+        ref={dotRef}
+        className="fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]"
+      ></div>
+    </div>
+  );
+};
+
+export default App;
